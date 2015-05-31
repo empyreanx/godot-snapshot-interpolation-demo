06266247a196da4e33fcb67ba6d3e5b7ec500169
James McLean <james@mcleanlabs.ca>
2015-05-31 02:06:24 -0400

    Refactoring. Comments.

--- README.md
+++ README.md
@@ -4,13 +4,16 @@ A simple networked physics demo for the Godot game engine.
 
 The server runs a simple physical simulation involving three boxes. Users can drag the boxes around on either the client or the server.
 
-The client uses an interpolation buffer to ensure animation remains smooth even in the face of bad network conditions..
+The client uses an interpolated buffer to ensure animation remains smooth even in the face of bad network conditions..
 
 ## Todo:
-* Add documentation to code
-* Add delta compression
-* More user-interface controls
-* Fix bugs:
-	- Division by zero in DelayBuffer
+* Configuration file
 * Add dedicated server script
-* Write UDP version (later)
+* Implement simple delta compression
+* Implement name-id cache
+* More user-interface controls
+* Fix bugs 
+* Write UDP version (based on EnhancedPacketPeer)
+
+##Known Bugs:
+* Potential divide by zero in KinematicBuffer
--- networking.gd
+++ networking.gd
@@ -2,7 +2,7 @@ extends Node
 
 var KinematicBuffer = load("kinematicbuffer.gd")
 
-const NETWORK_FPS = 1.0 / 10.0
+const FRAME_DURATION = 1.0 / 10.0
 const BUFFER_WINDOW = 0.15
 
 var interpolation = false
@@ -14,13 +14,18 @@ var connect_btn = null
 var port = null
 var ip = null
 
+# For server
 var server = TCP_Server.new()
 var peers = []
 
+# For client
 var stream_peer = StreamPeerTCP.new()
 var packet_peer = PacketPeerStream.new()
 
+# Boxes in the scene
 var boxes = null
+
+# Kinematic buffers for each of the boxes
 var buffers = {}
 
 func _ready():
@@ -36,6 +41,11 @@ func _ready():
 	packet_peer.set_stream_peer(stream_peer)
 	set_process(true)
  
+func load_defaults():
+	pass
+	
+
+# Toggle starting/stoping a server
 func _on_start_pressed():
 	if (not ready):
 		if (server.listen(port.get_val()) != OK):
@@ -52,6 +62,7 @@ func _on_start_pressed():
 		connect_btn.set_disabled(false)
 		
 
+# Toggle connecting/disconnecting a client
 func _on_connect_pressed():
 	if (not ready):
 		if (stream_peer.connect(ip.get_text(), port.get_val()) != OK):
@@ -69,6 +80,7 @@ func _on_connect_pressed():
 		start_btn.set_disabled(false)
 		
 func _process(delta):
+	#Server update
 	if (ready and host):
 		while (server.is_connection_available()):
 			var stream_peer = server.take_connection()
@@ -76,7 +88,9 @@ func _process(delta):
 			packet_peer.set_stream_peer(stream_peer)
 			peers.append({ stream = stream_peer, packet = packet_peer })
 		
-		if (timer < NETWORK_FPS):
+		# After waiting (to simulate network less than ideal network conditions),
+		# set a snapshot
+		if (timer < FRAME_DURATION):
 			timer += delta
 		else:
 			timer = 0
@@ -85,6 +99,7 @@ func _process(delta):
 					if (peer.stream.is_connected()):
 						peer.packet.put_var([box.get_name(), box.get_rot(), box.get_pos()])
 		
+		# Handle input
 		for peer in peers:
 			while (peer.packet.get_available_packet_count() > 0):
 				var data = peer.packet.get_var()
@@ -95,8 +110,11 @@ func _process(delta):
 					box.drag(data[2])
 				elif (type == "stop_drag"):
 					box.stop_dragging()
-		
+	
+	#Client update
 	if (ready and not host and stream_peer.is_connected()):
+		# Read snapshots from server and add it to a kinematic buffer (if interpolating),
+		# or immediately update the local state (if not interpolating)
 		while (packet_peer.get_available_packet_count() > 0):
 			var data = packet_peer.get_var()
 			var name = data[0]
@@ -109,7 +127,8 @@ func _process(delta):
 				var box = get_node("boxes/" + name)
 				box.set_pos(pos)
 				box.set_rot(rot)
-		
+				
+		# Update interpolation and local state
 		if (interpolation):
 			for box in boxes:
 				var buffer = buffers[box.get_name()]
@@ -117,6 +136,7 @@ func _process(delta):
 				box.set_pos(buffer.get_pos())
 				box.set_rot(buffer.get_rot())
 
+# Toggle interpolation
 func _on_lerp_toggled(pressed):
 	interpolation = pressed
 	
@@ -124,6 +144,7 @@ func _on_lerp_toggled(pressed):
 		for box in boxes:
 			buffers[box.get_name()].reset()
 
+# Start/stop functions for client/server
 func start_client():
 	set_host_boxes(false)
 	toggle_kinematic_boxes(true)
